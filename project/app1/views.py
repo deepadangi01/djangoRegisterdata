95ece6da9a9dd05d862427c1d6b88e568ff23712
deepadangi01 <dangideepa1999@gmail.com>
2024-09-18 18:24:56 -1200

    update project

--- project/app1/views.py
+++ project/app1/views.py
@@ -6,13 +6,36 @@ def home(request):
 
 def register(request):
     return render(request,'register.html')
+
 def login(request):
     if request.method=='POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-        print(email,password)        
+        name1=request.COOKIES['name'] 
+        contact1=request.COOKIES['contact']
+        email1=request.COOKIES['email'] 
+        password1=request.COOKIES['password'] 
+        print(name1,contact1,email,password1)
+        if email==email1:
+            if password1==password:
+                data={
+                    'name':name1,
+                    'email':email1,
+                    'contact':contact1,
+                    'pass':password1
+                    }
+                return render(request,'dashboard.html',data)
+                # return render(request,'first.html',data)
+            else:
+                msg="email & password not matched "
+                return render(request,'login.html',{'msg':msg})
+        else:
+                msg="email not register "
+                return render(request,'login.html',{'msg':msg})
+ 
     else:
-       return render(request,'login.html')
+       msg="Welcome to login page"
+       return render(request,'login.html',{'msg':msg})
 
 def rdata(request):
     print(request.method)
@@ -29,7 +52,7 @@ def rdata(request):
     # print(password)
     response = render(request,'login.html')
     response.set_cookie('name',name)
-    response.set_cookie('conatct',contact)
+    response.set_cookie('contact',contact)
     response.set_cookie('email',email)
     response.set_cookie('password',password)
     return response
